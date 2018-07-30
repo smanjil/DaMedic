70f62de7ff4341977ddce3af504b7b66a74292dd
Peerity <jan@peerity.io>
2018-07-30 17:55:24 +0200

    csv joined, json created

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+import os
 import pandas as pd
 
 from config import FILE_PATH
@@ -11,8 +12,15 @@ class CSVJoiner(object):
         self.df2 = df2
 
     def join_dfs(self):
-        print (self.df1.head())
-        print (self.df2.head())
+        def get_id_val_df2(row):
+            return self.df2[self.df2['id'] == row]['val2'].values
+
+        self.df1['val2'] = self.df1['id'].apply(get_id_val_df2)
+
+    def get_json(self):
+        if 'output_file' not in os.listdir(FILE_PATH):
+            os.mkdir('output_file')
+        self.df1.to_json('/'.join([FILE_PATH, 'output_file/result.json']), orient = 'records')
 
 
 if __name__ == '__main__':
@@ -22,3 +30,4 @@ if __name__ == '__main__':
     # instantiate CSVJoiner class
     csvjoin = CSVJoiner(df1, df2)
     csvjoin.join_dfs()
+    csvjoin.get_json()
